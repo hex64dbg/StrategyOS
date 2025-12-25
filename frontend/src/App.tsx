69bed7eac518f9e33d299cfa32a42eda8a1f9d7b
guidra-rev <guidra@protonmail.com>
2025-12-25 22:23:41 +0100

    feat(frontend): add MUI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,25 @@
+import { Box, Container, createTheme, CssBaseline, ThemeProvider, Typography } from '@mui/material'
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+})
+
 export default function App() {
   return (
-    <div>
-      <h1>StrategyOS</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Box sx={{ py: 4 }}>
+          <Typography
+            variant="h2"
+            component="h1"
+          >
+            StrategyOS
+          </Typography>
+        </Box>
+      </Container>
+    </ThemeProvider>
   )
 }
